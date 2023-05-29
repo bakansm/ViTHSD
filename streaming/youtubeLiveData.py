c81ab31f65babcc68cdf4e1fd3706a5b752a8d42
bakansm <khuynhbao5@gmail.com>
2023-05-29 21:06:02 +0700

    update

--- streaming/youtubeLiveData.py
+++ streaming/youtubeLiveData.py
@@ -5,7 +5,7 @@ from kafkaHelper import initProducer, produceRecord
 
 producer = initProducer()
 TOPIC = 'youtube'
-VID_ID = "yQ3zrHaBAKA"
+VID_ID = "xvIoUJu3GkY"
 
 # Streaming Data
 chat = pytchat.create(video_id=VID_ID)
@@ -14,16 +14,18 @@ if(chat.is_alive()):
     print("Livestream chat connected successfully")
     while chat.is_alive():
         for raw_data in chat.get().sync_items():
-            
+            start = time.time()
             predict_data = predict(raw_data.message)
-
+            end = time.time()
+            predict_time = start-end
             data = {
                 'timestamp': raw_data.timestamp,
                 'datetime': raw_data.datetime, 
-                'userid': raw_data.author.channelId,
-                'username': raw_data.author.name,
+                # 'userid': raw_data.author.channelId,
+                # 'username': raw_data.author.name,
                 'message': raw_data.message,
-                'predict': predict_data
+                'predict': predict_data,
+                'predict-time': predict_time,
             }
 
             produceRecord(data, producer, TOPIC)
